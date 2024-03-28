89ce4b3d3fa42f93a51aa6731161954ccf2e72a1
Ruxo Zheng <rz@just.sent.as>
2024-03-28 07:49:05 +0700

    Add Music Store View

---
+++ src/Avalonia.MusicStore/Views/MusicStoreView.axaml.fs
@@ -0,0 +1,13 @@
+namespace Avalonia.MusicStore
+
+open Avalonia
+open Avalonia.Controls
+open Avalonia.Markup.Xaml
+
+type MusicStoreView () as this = 
+    inherit UserControl ()
+
+    do this.InitializeComponent()
+
+    member private this.InitializeComponent() =
+        AvaloniaXamlLoader.Load(this)
