6bb1178ea3a505362cd4f08d16f1c1be979976f7
Ruxo Zheng <rz@just.sent.as>
2024-03-30 07:36:05 +0700

    Search iTune library and show artist/title

---
+++ src/Avalonia.MusicStore/Models/Models.fs
@@ -0,0 +1,23 @@
+﻿namespace Avalonia.MusicStore.Models
+
+open iTunesSearch.Library
+
+type Album = {
+    Artist: string
+    Title: string
+    CoverUrl: string
+}
+
+module Album =
+    let private SearchManager = iTunesSearchManager()
+
+    let searchAsync search_term =
+        async {
+            let! query = search_term |> SearchManager.GetAlbumsAsync |> Async.AwaitTask
+            return seq {
+                for x in query.Albums do
+                    { Artist = x.ArtistName
+                      Title = x.CollectionName
+                      CoverUrl = x.ArtworkUrl100 }
+            }
+        }
--- src/Avalonia.MusicStore/ViewModels/Models.fs
+++ src/Avalonia.MusicStore/ViewModels/Models.fs
@@ -1,28 +1,54 @@
 ﻿namespace Avalonia.MusicStore.ViewModels
 
+open System
 open System.Collections.ObjectModel
+open FSharp.Control.Reactive
 open ReactiveUI
+open Avalonia.MusicStore.Models
 
 type ViewModelBase() =
     inherit ReactiveObject()
 
-type AlbumViewModel() =
+type AlbumViewModel(album: Album) =
     inherit ViewModelBase()
 
-type MusicStoreViewModel() =
+    member this.Artist = album.Artist
+    member this.Title = album.Title
+
+type MusicStoreViewModel() as this =
     inherit ViewModelBase()
 
     let search_results = ObservableCollection<AlbumViewModel>()
-    do search_results.Add <| AlbumViewModel()
-    do search_results.Add <| AlbumViewModel()
-    do search_results.Add <| AlbumViewModel()
 
-    let mutable search_text :string option = None
+    let mutable search_text = String.Empty
     let mutable is_busy = false
     let mutable selected_album :AlbumViewModel option = None
+
+    let notNull = Option.ofObj >> Option.filter (not << String.IsNullOrWhiteSpace)
+
+    let doSearch s = async {
+        is_busy <- true
+        search_results.Clear()
+
+        match notNull s with
+        | Some v ->
+            let! albums = v |> Album.searchAsync
+            albums |> Seq.iter (AlbumViewModel >> search_results.Add)
+        | None -> ()
+
+        is_busy <- false
+    }
+
+    do this.Init()
 
     member this.SearchText with get() = search_text and set value = this.RaiseAndSetIfChanged(&search_text, value) |> ignore
     member this.IsBusy with get() = is_busy and set value = this.RaiseAndSetIfChanged(&is_busy, value) |> ignore
 
     member this.SelectedAlbum with get() = selected_album and set value = this.RaiseAndSetIfChanged(&selected_album, value) |> ignore
     member this.SearchResults with get() = search_results
+
+    member private this.Init() =
+        this.WhenAnyValue(fun x -> x.SearchText)
+            |> Observable.throttle (400 |> TimeSpan.FromMilliseconds)
+            |> Observable.subscribe (doSearch >> Async.Start)
+            |> ignore
